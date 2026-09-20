55bcaf4f8cc3027f3630d17409e61215fe218b00
Gabriela Dacayana <gabrieladacayana@gmail.com>
2026-09-20 12:23:34 +0800

    added gamePanel, player, and level files

---
+++ src/GamePanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+
+    Player player;
+    Level level;
+    Timer timer;
+    boolean timing;
+
+    public void run(){
+
+    }
+
+}
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,3 +1,14 @@
-public class GameWindow {
 
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame{
+    GamePanel gamePanel = new GamePanel();
+
+    public GameWindow(){
+        setTitle("Super Mario");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
 }
